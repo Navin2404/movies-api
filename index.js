9d67a75a0db35d10008c6b533a366c7be4333614
Navin <navindena2432000@gmail.com>
2025-08-20 11:59:29 +0530

    route structure

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-import express from 'express';
+(import express from 'express';
 
 const app = express()
 const PORT = 6969;
 
 app.get('/', (req,res)=>{
     res.json({msg: "this is get route..."})
+})
+
+// crud functionalities - rest api
+
+// for getting movie list
+app.get('/movies', (req,res)=> {
+
+})
+
+// for posting new movie
+app.post('/movies', (req,res)=>{
+
+})
+
+// for updating old detail
+app.put('/movies/:id', (req,res)=>{
+
+})
+
+// for deleting old record
+app.delete('/moives/:id', (req,res)=>{
+
 })
 
 
